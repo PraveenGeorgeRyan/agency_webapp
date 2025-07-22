fcded7f9113a7613c95c1d9aed9162621bd83d71
Praveen George Ryan <p.g.ryan333@gmail.com>
2025-07-22 21:06:16 +0530

    Add Global Header and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import AboutSection from '@/components/AboutSection';
-import Footer from '@/components/Footer';
-import GettingStartedSection from '@/components/GettingStartedSection';
-import Header from '@/components/Header';
-import HeroSection from '@/components/HeroSection';
-import LearnMoreSection from '@/components/LearnMoreSection';
-import PartnersSection from '@/components/PartnersSection';
-import ReviewsSection from '@/components/ReviewsSection';
-import ServicesSection from '@/components/ServicesSection';
+import AboutSection from "@/components/AboutSection";
+import GettingStartedSection from "@/components/GettingStartedSection";
+import HeroSection from "@/components/HeroSection";
+import LearnMoreSection from "@/components/LearnMoreSection";
+import PartnersSection from "@/components/PartnersSection";
+import ReviewsSection from "@/components/ReviewsSection";
+import ServicesSection from "@/components/ServicesSection";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
+    <>
       <HeroSection />
       <AboutSection />
       <ServicesSection />
@@ -19,7 +16,6 @@ export default function Home() {
       <ReviewsSection />
       <GettingStartedSection />
       <PartnersSection />
-      <Footer />
-    </main>
+    </>
   );
 }
